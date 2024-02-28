125ff9a9f4aabe380cffd4d18f6f864abc2e7653
hrmasss <hojayfarahman@gmail.com>
2024-02-28 22:12:29 +0600

    redirect based on auth

--- src/middlewares/redirects.ts
+++ src/middlewares/redirects.ts
@@ -1,11 +1,22 @@
 import type { NextRequest, NextMiddleware, NextFetchEvent } from "next/server";
 import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export function withRedirects(middleware: NextMiddleware) {
   return async (request: NextRequest, event: NextFetchEvent) => {
     // Redirect to next auth sign in page
     if (request.nextUrl.pathname === "/login") {
       return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+    }
+
+    const user = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    // Redirect authenticated users to the app
+    if (request.nextUrl.pathname === "/" && user) {
+      return NextResponse.redirect(new URL("/app", request.url));
     }
 
     return middleware(request, event);
