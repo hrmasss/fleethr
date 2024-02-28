51353a54849370536e73e373268b00de9aa9cee3
hrmasss <hojayfarahman@gmail.com>
2024-02-28 23:44:31 +0600

    add condition to app page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,19 +1,29 @@
-"use client";
-
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+import { db } from "@/server/db";
 import { Button } from "@/components/ui/button";
-import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
-export default function Dashboard() {
-  const { data: session, status } = useSession();
+export default async function Dashboard() {
+  const session = await getServerAuthSession();
+
+  if (!session?.user) redirect("/login");
+
+  const userOrganizationsCount = await db.user.findUnique({
+    where: { id: session?.user.id },
+    select: { _count: { select: { organizations: true } } },
+  });
+
+  if (!userOrganizationsCount?._count.organizations)
+    redirect("/app/get-started");
+
   return (
     <main className="flex flex-col justify-center gap-4 px-2 py-8 lg:px-24">
       <h1 className="border-b py-4 text-5xl">Dashboard</h1>
       <div>
-        <h2 className="text-3xl">
-          {status === "loading" ? "loading..." : `Hello, ${session?.user.name}`}
-        </h2>
-        <Button className="mt-4" onClick={() => signOut()}>
-          Sign out
+        <h2 className="text-3xl">Hello, {session.user.name}</h2>
+        <Button asChild className="mt-4">
+          <Link href="/api/auth/signout">Sign out</Link>
         </Button>
       </div>
     </main>
