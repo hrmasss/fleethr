bc26d58272d895c14618857dd2c50ee1e546e0f5
hrmasss <hojayfarahman@gmail.com>
2024-03-01 19:54:22 +0600

    add create procedure for organization

--- src/server/api/routers/organization.ts
+++ src/server/api/routers/organization.ts
@@ -3,7 +3,29 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
-  publicProcedure,
 } from "@/server/api/trpc";
 
-export const organizationRouter = createTRPCRouter({});
+const organizationData = z.object({
+  name: z.string().min(1).max(255),
+  maxSize: z.number().int().min(20).max(1000),
+  description: z.string().min(12),
+});
+
+export const organizationRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(organizationData)
+    .mutation(async ({ ctx, input }) => {
+      // Create the organization
+      const organization = await ctx.db.organization.create({
+        data: input,
+      });
+
+      // Update user role to SUPERADMIN [Complete control to the creator of the organization]
+      await ctx.db.user.update({
+        where: { id: ctx.session.user.id },
+        data: { role: "SUPERADMIN", organizationId: organization.id },
+      });
+
+      return organization;
+    }),
+});
