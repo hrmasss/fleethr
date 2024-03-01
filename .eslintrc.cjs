c23bc28820b0a8a58181853d75f1d478ab22728b
hrmasss <hojayfarahman@gmail.com>
2024-03-01 09:53:16 +0600

    fix eslint issue with shadcn components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,6 +31,10 @@ const config = {
         checksVoidReturn: { attributes: false },
       },
     ],
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      { allowSingleExtends: true },
+    ],
   },
 };
 
