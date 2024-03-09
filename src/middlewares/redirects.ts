0f55f056a1c438c24c44b900f0f7848e5c3fe660
hrmasss <hojayfarahman@gmail.com>
2024-03-09 10:23:24 +0600

    add middleware for /app

--- src/middlewares/redirects.ts
+++ src/middlewares/redirects.ts
@@ -4,6 +4,11 @@ import { getToken } from "next-auth/jwt";
 
 export function withRedirects(middleware: NextMiddleware) {
   return async (request: NextRequest, event: NextFetchEvent) => {
+    // Map /login to /signin
+    if (request.nextUrl.pathname === "/login") {
+      return NextResponse.rewrite(new URL("/signin", request.url));
+    }
+
     const user = await getToken({
       req: request,
       secret: process.env.NEXTAUTH_SECRET,
@@ -12,6 +17,10 @@ export function withRedirects(middleware: NextMiddleware) {
     // Redirect authenticated users to the app
     if (request.nextUrl.pathname === "/" && user) {
       return NextResponse.redirect(new URL("/app", request.url));
+    }
+
+    if (request.nextUrl.pathname.startsWith("/app") && !user) {
+      return NextResponse.redirect(new URL("/signin", request.url));
     }
 
     return middleware(request, event);
