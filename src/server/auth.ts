43f94dd0f404950a04e50dabba9b29a8f5b36ecc
hrmasss <hojayfarahman@gmail.com>
2024-05-07 21:16:00 +0600

    add role & auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,14 @@
+import { type Adapter } from "next-auth/adapters";
+import type { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compare } from "bcryptjs";
+import { db } from "@/server/db";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
-
-import { env } from "@/env";
-import { db } from "@/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -20,15 +20,20 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
+      role: UserRole;
     } & DefaultSession["user"];
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+  interface User {
+    role: UserRole;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    role: UserRole;
+  }
 }
 
 /**
@@ -37,20 +42,63 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    session: async ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token?.sub,
+          role: token.role,
+        },
+      };
+    },
+
+    jwt: async ({ token }) => {
+      const userRole = await getUserRole(token.sub);
+
+      token.role = userRole;
+      return token;
+    },
   },
   adapter: PrismaAdapter(db) as Adapter,
+  pages: {
+    // signIn: "/login",
+    // signOut: "/logout",
+  },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      // @ts-expect-error Next auth issues with strict mode (See issue #2701 on github)
+      authorize: async (credentials) => {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await db.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user?.password) return null;
+
+        const isValidPassword = await compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isValidPassword) return null;
+
+        return {
+          ...user,
+          password: null,
+        };
+      },
     }),
     /**
      * ...add more providers here.
@@ -70,3 +118,17 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = () => getServerSession(authOptions);
+
+// Utility function for getting user role
+export async function getUserRole(userId?: string): Promise<UserRole> {
+  "use server";
+
+  if (!userId) return "BASE";
+
+  const user = await db.user.findUnique({
+    where: { id: userId },
+    select: { role: true },
+  });
+
+  return user?.role ?? "BASE";
+}
