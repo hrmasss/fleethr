45dcf1b3622c7272b843b527bf0cf01b55d7ba5e
hrmasss <hojayfarahman@gmail.com>
2024-05-07 23:08:33 +0600

    add footer component

--- src/app/(www)/layout.tsx
+++ src/app/(www)/layout.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <section className="flex-1 lg:px-12 xl:px-32">{children}</section>
+      <Footer />
     </div>
   );
 }
