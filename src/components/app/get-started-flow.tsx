2a9770533b3a098fad9b8712a67340d65a8dae3c
hrmasss <hojayfarahman@gmail.com>
2024-05-11 05:51:26 +0600

    [deploy] update get started flow

--- src/components/app/get-started-flow.tsx
+++ src/components/app/get-started-flow.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { useState } from "react";
-import { Stepper } from "@mantine/core";
+import { useState, useEffect } from "react";
+import { Stepper, Box, Text, Button } from "@mantine/core";
+import { IconRosetteDiscountCheck } from "@tabler/icons-react";
 import { CreateAccountForm } from "@/components/app/create-account-form";
 import { CreateOrganizationForm } from "@/components/app/create-organization-form";
 import { CreateSubscriptionForm } from "@/components/app/create-subscription-form";
+import { publicLinks } from "@/lib/nav-data";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface Props {
   step?: number;
@@ -12,9 +16,14 @@ interface Props {
 
 export default function GetStartedFlow({ step }: Props) {
   const [active, setActive] = useState(step ?? 0);
+  const router = useRouter();
 
   const nextStep = () =>
     setActive((current) => (current < 3 ? current + 1 : current));
+
+  useEffect(() => {
+    if (active === 3) router.push(publicLinks.dashboard);
+  }, [active, router]);
 
   return (
     <Stepper
@@ -32,7 +41,31 @@ export default function GetStartedFlow({ step }: Props) {
         <CreateSubscriptionForm onSuccess={nextStep} className="mt-2" />
       </Stepper.Step>
 
-      <Stepper.Completed>Completed!</Stepper.Completed>
+      <Stepper.Completed>
+        <div className="mt-4 flex flex-col items-center gap-4 text-center md:mt-12">
+          <Box c="teal">
+            <IconRosetteDiscountCheck className="size-32" />
+          </Box>
+
+          <h3 className="text-2xl font-bold md:text-4xl">
+            Onboarding complete
+          </h3>
+
+          <Text c="dimmed" className="text-sm sm:text-base">
+            Heading to the dashboard. If not automatically redirected within a
+            few seconds, please use the button below.
+          </Text>
+
+          <Button
+            mt="sm"
+            variant="gradient"
+            component={Link}
+            href={publicLinks.dashboard}
+          >
+            Go to dashboard
+          </Button>
+        </div>
+      </Stepper.Completed>
     </Stepper>
   );
 }
