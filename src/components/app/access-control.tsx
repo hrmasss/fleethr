bb6425b965f98b6655aa416170a46bf30f69e50f
hrmasss <hojayfarahman@gmail.com>
2024-05-15 23:16:30 +0600

    address permission issues for dynamic routes

--- src/components/app/access-control.tsx
+++ src/components/app/access-control.tsx
@@ -10,7 +10,23 @@ interface Props {
 export default function AccessControl({ children, permittedRoutes }: Props) {
   const requestedPath = usePathname();
   if (!requestedPath) notFound();
-  if (!permittedRoutes.includes(requestedPath)) notFound();
+
+  const isRoutePermitted = permittedRoutes.some((route) => {
+    /**
+     * ? Replace any dynamic segment (e.g., [id]) with a regular expression pattern
+     * ? ([^/]+) that matches one or more non-slash characters
+     *
+     * * If /foo/[id] is in permitted routes
+     * * /foo/123 will pass the regEx
+     * * /foo/123/bar will not
+     */
+    const routeRegex = new RegExp(
+      `^${route.replace(/\[(.*?)\]/g, "([^/]+)")}$`,
+    );
+    return routeRegex.test(requestedPath);
+  });
+
+  if (!isRoutePermitted) notFound();
 
   return <>{children}</>;
 }
