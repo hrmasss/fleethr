0013c5e0fd4222498c70977cb39b8d11706c64b7
hrmasss <hojayfarahman@gmail.com>
2024-06-04 11:42:48 +0600

    [deploy] add mock dashboard

--- src/app/(app)/app/page.tsx
+++ src/app/(app)/app/page.tsx
@@ -1,7 +1,48 @@
+import { BarChart } from "@mantine/charts";
+import DashboardStats from "@/components/app/dashboard-stats";
+
+const data = [
+  { month: "January", Employee: 12, Admin: 9, Others: 2 },
+  { month: "February", Employee: 19, Admin: 12, Others: 4 },
+  { month: "March", Employee: 4, Admin: 10, Others: 2 },
+  { month: "April", Employee: 10, Admin: 2, Others: 8 },
+  { month: "May", Employee: 8, Admin: 14, Others: 12 },
+  { month: "June", Employee: 7, Admin: 6, Others: 10 },
+];
+
 export default function Page() {
   return (
-    <main className="flex h-full flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold lg:text-5xl">Dashboard</h1>
+    <main>
+      <div>
+        <h3 className="my-2 text-xl font-bold md:text-3xl">Dashboard</h3>
+
+        <p className="max-w-xl">
+          Here you can find some statistics about your organization.
+        </p>
+
+        <div className="my-8">
+          <DashboardStats />
+        </div>
+      </div>
+
+      <div>
+        <h3 className="mt-8 text-lg font-bold md:text-xl">
+          Employee History
+        </h3>
+
+        <BarChart
+          h={300}
+          data={data}
+          withLegend
+          dataKey="month"
+          series={[
+            { name: "Employee", color: "violet.6" },
+            { name: "Admin", color: "blue.6" },
+            { name: "Others", color: "teal.6" },
+          ]}
+          tickLine="y"
+        />
+      </div>
     </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import "@mantine/charts/styles.css";
 import '@mantine/dates/styles.css';
 import "@mantine/tiptap/styles.css";
 import { Notifications } from "@mantine/notifications";
